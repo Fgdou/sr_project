6da6e60d16f1e7d817d9f36428936ab1454a223e
Fgdou <fabigoardou@gmail.com>
2024-01-25 13:19:50 +0100

    add websocket lib backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use tokio::net::TcpListener;
+use web_socket::*;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let mut listener = TcpListener::bind("0.0.0.0:8080").await;
+    println!("Websocket server started on ws://0.0.0.0:8080");
+
+    loop {
+    }
 }
