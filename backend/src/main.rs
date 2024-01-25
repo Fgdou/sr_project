07321577ca1d9d4526f8f9db7a51ffb55b646a5d
fgdou <fabigoardou@gmail.com>
2024-01-25 13:47:31 +0100

    impl default websocket listener

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,21 @@
 use tokio::net::TcpListener;
-use web_socket::*;
+use tokio_websockets::{Error, ServerBuilder};
+use futures_util::{SinkExt, StreamExt};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     println!("Hello, world!");
 
-    let mut listener = TcpListener::bind("0.0.0.0:8080").await;
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
     println!("Websocket server started on ws://0.0.0.0:8080");
 
-    loop {
+    let (conn, _) = listener.accept().await?;
+    let mut server = ServerBuilder::new().accept(conn).await?;
+
+    while let Some(Ok(item)) = server.next().await {
+        println!("Received: {item:?}");
+        server.send(item).await?;
     }
+
+    Ok(())
 }
