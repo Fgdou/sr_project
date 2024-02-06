3003be21cc5312df4836aece34dea89d3473ea5a
fgdou <fabigoardou@gmail.com>
2024-02-06 21:39:21 +0100

    test for client

--- backend/src/client.rs
+++ backend/src/client.rs
@@ -4,13 +4,23 @@ use websocket::{sync::Writer, OwnedMessage};
 
 use crate::objects::{Direction, MessageServer, Player, Vector2};
 
-pub struct Client {
+pub struct Client<T> {
     player: Player,
-    writer: Writer<TcpStream>,
+    writer: T,
     next_move: Vec<Direction>,
 }
 
-impl Client {
+pub trait WriterInterface {
+    fn send_message(&mut self, message: &OwnedMessage);
+}
+
+impl WriterInterface for Writer<TcpStream> {
+    fn send_message(&mut self, message: &OwnedMessage) {
+        let _ = Writer::send_message(self, message);
+    }
+}
+
+impl<T: WriterInterface> Client<T> {
     pub fn send_message(&mut self, message: &MessageServer) {
         match message {
             MessageServer::Error(error) => println!("Error for {} : {}", self.player.username(), error),
@@ -18,7 +28,7 @@ impl Client {
         }
         self.send_raw_message(&message.into());
     }
-    pub fn new(player: Player, writer: Writer<TcpStream>) -> Self {
+    pub fn new(player: Player, writer: T) -> Self {
         Self {
             player,
             writer,
@@ -48,5 +58,46 @@ impl Client {
             self.player.set_direction(dir.clone());
         }
         self.player.update(size)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Writer;
+    impl WriterInterface for Writer {
+        fn send_message(&mut self, _: &websocket::OwnedMessage) {}
+    }
+
+    fn client_example() -> Client<Writer> {
+        Client::new(Player::new(0), Writer{})
+    }
+
+    #[test]
+    fn player_move_empty() {
+        let mut client = client_example();
+        assert!(client.get_next_move().is_none());
+    }
+
+    #[test]
+    fn player_move_one() {
+        let mut client = client_example();
+        client.add_next_move(Direction::Down);
+        assert_eq!(Some(Direction::Down), client.get_next_move());
+    }
+
+    #[test]
+    fn player_move_five() {
+        let mut client = client_example();
+        client.add_next_move(Direction::Down);
+        client.add_next_move(Direction::Up);
+        client.add_next_move(Direction::Left);
+        client.add_next_move(Direction::Right);
+        client.add_next_move(Direction::Down);
+        assert_eq!(Some(Direction::Up), client.get_next_move());
+        assert_eq!(Some(Direction::Left), client.get_next_move());
+        assert_eq!(Some(Direction::Right), client.get_next_move());
+        assert_eq!(Some(Direction::Down), client.get_next_move());
     }
 }
