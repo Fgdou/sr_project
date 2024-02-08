3c41259189cd3b98d91d167386e95d221fbf1993
Fgdou <fabigoardou@gmail.com>
2024-02-08 14:13:30 +0100

    finish loadtesting

--- loadtesting/src/client.rs
+++ loadtesting/src/client.rs
@@ -2,39 +2,57 @@ use std::{sync::{Arc, Mutex}, thread::{self, JoinHandle}, time::Duration};
 
 use websocket::{ClientBuilder, OwnedMessage};
 
+use chrono::Local;
+
 pub struct Client {
     failed: Arc<Mutex<bool>>,
     thread: JoinHandle<()>,
+    average: Arc<Mutex<Vec<i64>>>
 }
 
 impl Client {
     pub fn new(id: i32, speed_ms: i32) -> Client {
         let failed = Arc::new(Mutex::new(false));
         let failed_copy = failed.clone();
+        let average = Arc::new(Mutex::new(Vec::new()));
+        let average_copy = average.clone();
         Client {
             thread: thread::spawn(move || {
-                let res = Client::run(id, failed_copy.clone(), speed_ms);
+                let res = Client::run(id, failed_copy.clone(), speed_ms, average_copy);
                 if res.is_none() {
                     let _ = failed_copy.lock().map(|mut f| *f = true);
                 }
             }),
             failed,
+            average
         }
     }
     pub fn has_failed(&self) -> bool {
         self.failed.lock().map(|f| *f).unwrap_or(true)
     }
+    pub fn average(&self) -> Option<i64> {
+        let list = self.average.lock().ok()?;
+        if list.is_empty() {
+            Some(0)
+        } else {
+            Some(list.iter().sum::<i64>() / list.len() as i64)
+        }
+    }
     pub fn join(self) {
         let _ = self.failed.lock().map(|mut f| *f = true);
         let _ = self.thread.join();
     }
-    fn run(id: i32, failed: Arc<Mutex<bool>>, speed_ms: i32) -> Option<()> {
+    fn run(id: i32, failed: Arc<Mutex<bool>>, speed_ms: i32, diffs: Arc<Mutex<Vec<i64>>>) -> Option<()> {
         let mut client = ClientBuilder::new("ws://localhost:8080")
             .unwrap().connect(None).ok()?;
 
         let message = OwnedMessage::Text(format!(r#"{{"Connection": "l{}"}}"#, id).to_string());
 
         client.send_message(&message).ok()?;
+
+        let _ = client.set_nonblocking(true);
+
+        let mut last_time = Local::now();
 
         while !failed.lock().map(|f| *f).unwrap_or(false) {
             let messages = vec![
@@ -46,7 +64,25 @@ impl Client {
 
             for m in messages {
                 client.send_message(&m).ok()?;
-                thread::sleep(Duration::from_millis(speed_ms as u64));
+
+                for _ in 0..10 {
+                    if let Ok(mut diffs) = diffs.lock() {
+                        if let Ok(OwnedMessage::Text(message)) = client.recv_message() {
+                            
+                            if message.starts_with(r#"{"ChangeInfos":"#) {
+                                let diff = Local::now().signed_duration_since(&last_time).num_milliseconds();
+                                diffs.push(diff);
+                                last_time = Local::now();
+    
+                                while diffs.len() > 10 {
+                                    diffs.remove(0);
+                                }
+                            }
+                        }
+                    }
+                    thread::sleep(Duration::from_millis(speed_ms as u64/10));
+                }
+
             }
         };
         Some(())
--- loadtesting/src/main.rs
+++ loadtesting/src/main.rs
@@ -2,45 +2,67 @@ mod client;
 
 use std::{thread, time::Duration};
 
+use chrono::Local;
 use client::Client;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum State {
-    Growing,
-    Reducing,
-    Stable,
+    Growing(i32),
+    Stable{target: i32, t: i32},
 }
 
 fn main() {
-    let mut id = 0;
-    let mut clients =  Vec::new();
-    let mut state = State::Growing;
+    let mut id = 100;
+    let mut clients: Vec<Client> =  Vec::new();
+    let mut state = State::Growing(0);
+    let start = Local::now();
 
     loop {
+        let diff = Local::now().signed_duration_since(start);
+        if diff.num_seconds() > 60 {
+            state = State::Stable { target: clients.len() as i32, t: 0 }
+        }
+        
+        let count = clients.len();
+        clients.retain(|c| !c.has_failed());
+        
+        let average = if clients.is_empty() {0} else {
+            clients.iter()
+                .filter_map(|c| c.average())
+                .sum::<i64>() / clients.len() as i64
+        };
+        let failed = count - clients.len();
+
         match state {
-            State::Growing => {
-                clients.push(Client::new(id, 150));
+            State::Growing(_) => {
+                clients.push(Client::new(id, 500));
                 id += 1;
-            },
-            State::Reducing => {
-                if let Some(c) = clients.pop() {
-                    c.join();
+
+                if average > 400 {
+                    state = State::Stable{t: 0, target: clients.len() as i32};
                 }
             },
-            State::Stable => break,
+            State::Stable{target, t: _} => {
+                while (clients.len() as i32) < target {
+                    clients.push(Client::new(id, 150));
+                }
+            },
         }
 
-        let failed = clients.iter().filter(|c| c.has_failed()).count();
-
         state = match state {
-            State::Growing => if failed > 0 {State::Reducing} else {State::Growing},
-            State::Reducing => if failed > 0 {State::Reducing} else {State::Stable},
-            State::Stable => State::Stable,
+            State::Growing(n) => if n > 100 {State::Stable{target: clients.len() as i32, t: 0}} else {State::Growing(n + failed as i32 )},
+            State::Stable{t: 500, target} => {
+                println!("{} players {} ms", target, average);
+
+                break
+            },
+            State::Stable{target, t} => State::Stable{target, t: t+1},
         };
         
-        println!("State: {:?} clients: {}", state, clients.len());
-        thread::sleep(Duration::from_millis(10));
+        println!("State: {:?} clients: {} Ping: {}ms", state, clients.len(), average);
+        thread::sleep(Duration::from_millis(100));
     }
 
+    println!("Stopping");
     clients.into_iter().for_each(|c| c.join());
 }
